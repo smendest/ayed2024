9d55b6ce410b7cef088320c220d82d22bf4cbf31
smendest <smendestello@gmail.com>
2024-03-24 15:16:30 -0300

    First program println

--- src/App.java
+++ src/App.java
@@ -1,5 +1,8 @@
+import bin.tp1.ej1.Ejercicio1;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Ejercicio1.showNumbersFor(17, 21);
     }
 }
